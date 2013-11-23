27d4bc75b0716539ff8d843b3ab0c45435abaf2a
Shaohua Zhou <shao@versal.com>
2013-11-23 13:18:14 -0800

    yo backbone:model app

---
+++ frontend/app/scripts/models/app.js
@@ -0,0 +1,12 @@
+/*global frontend, Backbone*/
+
+frontend.Models = frontend.Models || {};
+
+(function () {
+    'use strict';
+
+    frontend.Models.AppModel = Backbone.Model.extend({
+
+    });
+
+})();
