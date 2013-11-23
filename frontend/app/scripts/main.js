180a057e7aeeea3ca4ef5dcd9c83336b86a5c229
Shaohua Zhou <shao@versal.com>
2013-11-23 13:15:28 -0800

    Add frontend backbone init

---
+++ frontend/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global frontend, $*/
+
+
+window.frontend = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    frontend.init();
+});
