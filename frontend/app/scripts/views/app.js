41fae07be4db7f2039d309c5f0b935c4ca13e869
Shaohua Zhou <shao@versal.com>
2013-11-24 11:49:37 -0800

    WIP for search page

--- frontend/app/scripts/views/app.js
+++ frontend/app/scripts/views/app.js
@@ -5,7 +5,7 @@ frontend.Views = frontend.Views || {};
 (function () {
   frontend.Views.AppView = Backbone.View.extend({
 
-    template: JST['app/scripts/templates/real_profile.hbs'],
+    template: JST['app/scripts/templates/search.hbs'],
 
     render: function(){
       this.$el.html(this.template());
