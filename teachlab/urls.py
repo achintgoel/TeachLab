572eaca60f179e04248f09a7308627f04edec38e
agoel <achintg12@gmail.com>
2013-11-24 14:14:43 -0800

    urls change

--- teachlab/urls.py
+++ teachlab/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'teachlab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'', include('social_auth.urls')),
+    url(r'^$', RedirectView.as_view(url='/accounts/profile')),
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^repository/', include('repository.urls', namespace="repository")),
     url(r'^admin/', include(admin.site.urls)),
